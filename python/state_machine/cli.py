0ca4dd4fd9c87464c00f4cedabcedff435e95d57
tristan <thuckabee.work@gmail.com>
2026-04-03 17:41:41 -0500

    basic functionality in statmach-py

--- python/state_machine/cli.py
+++ python/state_machine/cli.py
@@ -1,2 +1,21 @@
+import argparse
+
+def setup_command(args):
+  print("Running setup...")
+
 def main():
   print("Running Python version of State Machine...")
+
+  parser = argparse.ArgumentParser(prog="statmach-py")
+  subparsers = parser.add_subparsers(dest="command")
+
+  # Setup
+  setup_parser = subparsers.add_parser("setup", help="Setup Environment")
+  setup_parser.set_defaults(func=setup_command)
+
+
+  args = parser.parse_args()
+  if hasattr(args, "func"):
+    args.func(args)
+  else:
+    parser.print_help()
